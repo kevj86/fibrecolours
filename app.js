96c92958ac48f8986b355f7d694c5c4b3f05bf3b
kevj86 <109628697+kevj86@users.noreply.github.com>
2022-09-23 21:32:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const fibreNumber = document.getElementById("fibre-number");
+const elementNumber = document.getElementById("element-number");
+const elementCount = document.getElementById("element-count");
+const elementOther = document.getElementById("element-others");
+const numberResults = document.getElementById("number-results");
+const numberColour = document.getElementById("number-colour");
+const numberElement = document.getElementById("number-element");
+const numberOthers = document.getElementById("number-others");
+
+const maxElementFibre = 12;
+// prettier-ignore
+const fibreColours = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple", "black", "white", "pink", "aqua"]
+
+function getFibreNumber() {
+  const fibre = fibreNumber.value;
+  const elementValue = Math.ceil(fibre / 12);
+
+  let fibreColour = fibre < 13 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 12 - 1;
+  let coloursHtml = "";
+
+  for (let i = 0; i < fibreColours.length; i++) {
+    coloursHtml += `<span class="${fibreColours[i]}-fibre">${maxElementFibre * elementValue - (11 - i)}</span> `;
+  }
+
+  if (fibreNumber.value === "" || fibreNumber.value === "0") {
+  } else {
+    numberColour.innerHTML = `<div id="number-colour">Fibre ${fibre} is <span class="${fibreColours[fibreColour]}-fibre inline-span">${fibreColours[fibreColour]}</span></div>`;
+    numberElement.innerHTML = `<div id="number-element">Fibre is in ELEMENT ${elementValue}</div>`;
+    numberOthers.innerHTML = `<div id="number-others">${coloursHtml}</div>`;
+  }
+}
+
+function getElementNumber() {
+  //prettier-ignore
+  // let fibreColour = fibre < 13 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 12 - 1;
+  let coloursHtml = "";
+
+  for (let i = 0; i < fibreColours.length; i++) {
+    coloursHtml += `<span class="${fibreColours[i]}-fibre">${maxElementFibre * elementNumber.value - (11 - i)}</span> `;
+  }
+
+  elementCount.innerHTML = `<div id="element-Count">Fibre is in ELEMENT ${elementNumber.value}</div>`;
+  elementOther.innerHTML = `<div id="element-others">${coloursHtml}</div>`;
+}
+
+fibreNumber.addEventListener("keyup", getFibreNumber);
+elementNumber.addEventListener("keyup", getElementNumber);
