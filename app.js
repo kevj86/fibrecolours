70188c762a1e6374110a3e92063aa1ee8a498855
kevj86 <109628697+kevj86@users.noreply.github.com>
2022-09-24 21:17:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const numberResults = document.getElementById("number-results");
 const numberColour = document.getElementById("number-colour");
 const numberElement = document.getElementById("number-element");
 const numberOthers = document.getElementById("number-others");
+const resetButton = document.getElementById("reset");
 
 const maxElementFibre = 12;
 // prettier-ignore
@@ -23,8 +24,10 @@ function getFibreNumber() {
   }
 
   if (fibreNumber.value === "" || fibreNumber.value === "0") {
-  } else if (fibreNumber.value == "greg") {
-    document.getElementById("gregg").style.display = "block";
+  } else if (fibreNumber.value > 864) {
+    numberColour.innerHTML = `<div id="number-colour" class="alert"> MAX FIBRE COUNT 864</div>`;
+    numberElement.innerHTML = ``;
+    numberOthers.innerHTML = ``;
   } else {
     numberColour.innerHTML = `<div id="number-colour">Fibre ${fibre} is <span class="${fibreColours[fibreColour]}-fibre inline-span">${fibreColours[fibreColour]}</span></div>`;
     numberElement.innerHTML = `<div id="number-element">Fibre is in ELEMENT ${elementValue}</div>`;
@@ -41,9 +44,26 @@ function getElementNumber() {
     coloursHtml += `<span class="${fibreColours[i]}-fibre">${maxElementFibre * elementNumber.value - (11 - i)}</span> `;
   }
 
-  elementCount.innerHTML = `<div id="element-Count">Fibres ELEMENT ${elementNumber.value}</div>`;
-  elementOther.innerHTML = `<div id="element-others">${coloursHtml}</div>`;
+  if (elementNumber.value === "" || elementNumber === "0") {
+  } else if (elementNumber.value > 72) {
+    elementCount.innerHTML = `<div id="element-Count" class="alert">MAX ELEMENT COUNT 72</div>`;
+    elementOther.innerHTML = `<div id="element-others"></div>`;
+  } else {
+    elementCount.innerHTML = `<div id="element-Count">Fibres ELEMENT ${elementNumber.value}</div>`;
+    elementOther.innerHTML = `<div id="element-others">${coloursHtml}</div>`;
+  }
+}
+
+function resetContent() {
+  elementCount.innerHTML = ``;
+  elementOther.innerHTML = ``;
+  numberColour.innerHTML = ``;
+  numberElement.innerHTML = ``;
+  numberOthers.innerHTML = ``;
+  fibreNumber.value = ``;
+  elementNumber.value = ``;
 }
 
 fibreNumber.addEventListener("keyup", getFibreNumber);
 elementNumber.addEventListener("keyup", getElementNumber);
+resetButton.addEventListener("click", resetContent);
