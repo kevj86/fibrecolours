bfd6cae0f2a30c72f98a651ad2697456ca14d45f
kevj86 <109628697+kevj86@users.noreply.github.com>
2022-10-19 23:03:04 +0100

    added the addition of 8f Elements

--- app.js
+++ app.js
@@ -7,10 +7,16 @@ const numberColour = document.getElementById("number-colour")
 const numberElement = document.getElementById("number-element")
 const numberOthers = document.getElementById("number-others")
 const resetButton = document.getElementById("reset")
+const twelveFibre = document.getElementById("twelve-fibre")
+const eightFibre = document.getElementById("eight-fibre")
+
+let fibreSelect = 12
 
 const maxElementFibre = 12
+const maxElementFibreEight = 8
 // prettier-ignore
 const fibreColours = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple", "black", "white", "pink", "aqua"]
+const fibreColoursEight = ["blue", "orange", "green", "red", "slate", "yellow", "brown", "purple"]
 // function
 
 function getFibreNumber() {
@@ -36,6 +42,29 @@ function getFibreNumber() {
   }
 }
 
+function getFibreNumberEight() {
+  const fibre = fibreNumber.value
+  const elementValue = Math.ceil(fibre / 8)
+
+  let fibreColour = fibre < 9 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 8 - 1
+  let coloursHtml = ""
+
+  for (let i = 0; i < fibreColoursEight.length; i++) {
+    coloursHtml += `<span class="${fibreColoursEight[i]}-fibre">${maxElementFibreEight * elementValue - (7 - i)}</span> `
+  }
+
+  if (fibreNumber.value === "" || fibreNumber.value === "0") {
+  } else if (fibreNumber.value > 96) {
+    numberColour.innerHTML = `<div id="number-colour" class="alert"> MAX FIBRE COUNT 96</div>`
+    numberElement.innerHTML = ``
+    numberOthers.innerHTML = ``
+  } else {
+    numberColour.innerHTML = `<div id="number-colour">Fibre ${fibre} is <span class="${fibreColoursEight[fibreColour]}-fibre inline-span">${fibreColoursEight[fibreColour]}</span></div>`
+    numberElement.innerHTML = `<div id="number-element">Fibre is in ELEMENT ${elementValue}</div>`
+    numberOthers.innerHTML = `<div id="number-others">${coloursHtml}</div>`
+  }
+}
+
 function getElementNumber() {
   //prettier-ignore
   // let fibreColour = fibre < 13 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 12 - 1;
@@ -55,6 +84,26 @@ function getElementNumber() {
   }
 }
 
+function getElementNumberEight() {
+  //prettier-ignore
+  // let fibreColour = fibre < 13 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 12 - 1;
+  let coloursHtml = "";
+  
+
+  for (let i = 0; i < fibreColoursEight.length; i++) {
+    coloursHtml += `<span class="${fibreColoursEight[i]}-fibre">${maxElementFibreEight * elementNumber.value - (7 - i)}</span> `
+  }
+
+  if (elementNumber.value === "" || elementNumber === "0") {
+  } else if (elementNumber.value > 12) {
+    elementCount.innerHTML = `<div id="element-Count" class="alert">MAX ELEMENT COUNT 12</div>`
+    elementOther.innerHTML = `<div id="element-others"></div>`
+  } else {
+    elementCount.innerHTML = `<div id="element-Count">Fibres ELEMENT ${elementNumber.value}</div>`
+    elementOther.innerHTML = `<div id="element-others">${coloursHtml}</div>`
+  }
+}
+
 function resetContent() {
   elementCount.innerHTML = ``
   elementOther.innerHTML = ``
@@ -65,6 +114,36 @@ function resetContent() {
   elementNumber.value = ``
 }
 
-fibreNumber.addEventListener("keyup", getFibreNumber)
-elementNumber.addEventListener("keyup", getElementNumber)
+
+twelveFibre.addEventListener("click", function(){
+  fibreSelect = 12
+  resetContent()
+  eightFibre.classList.remove('active')
+  twelveFibre.classList.add('active')
+})
+
+eightFibre.addEventListener("click", function(){
+  fibreSelect = 8
+  resetContent()
+  eightFibre.classList.add('active')
+  twelveFibre.classList.remove('active')
+})
+
+
+fibreNumber.addEventListener("keyup", function() {
+    if (fibreSelect === 12) {
+      getFibreNumber()
+    }  else if(fibreSelect === 8) {
+      getFibreNumberEight()
+    }
+})
+
+elementNumber.addEventListener("keyup", function(){
+  if (fibreSelect === 12) {
+    getElementNumber()
+  }  else if(fibreSelect === 8) {
+    getElementNumberEight()
+  }
+})
+
 resetButton.addEventListener("click", resetContent)
