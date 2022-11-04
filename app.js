4d4190a806ed9abd5c1920e333b4e1bbc180f4c4
kevj86 <109628697+kevj86@users.noreply.github.com>
2022-11-04 22:08:08 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let elementSize = null;
+let fibreSelect = null;
 
 const maxElementFibre = 12;
 const maxElementFibreEight = 8;
@@ -12,6 +13,8 @@ const fibreNumber = document.getElementById("fibre-number");
 const elementNumber = document.getElementById("fibre-element");
 const displayFibre = document.getElementById("display-fibre");
 const displayElement = document.getElementById("display-element");
+const twelveFibre = document.getElementById("12f");
+const eightFibre = document.getElementById("8f");
 
 function getFibreNumber() {
   const fibre = fibreNumber.value;
@@ -38,6 +41,31 @@ function getFibreNumber() {
   }
 }
 
+function getFibreNumberEight() {
+  const fibre = fibreNumber.value;
+  const elementValue = Math.ceil(fibre / 8);
+
+  let fibreColour = fibre < 9 ? fibre / elementValue - 1 : fibre - (elementValue - 1) * 8 - 1;
+
+  let coloursHtml = fibreColoursEight
+    .map((fibreColour, index) => {
+      return `<span class="fibre-span ${fibreColour}-fibre">${maxElementFibreEight * elementValue - (7 - index)}</span> `;
+    })
+    .join("");
+
+  if (fibreNumber.value === "" || fibreNumber.value === "0") {
+  } else if (fibreNumber.value > 96) {
+    displayFibre.innerHTML = `
+        <div class="alert">MAX FIBRE COUNT 96</div>
+    `;
+  } else {
+    displayFibre.innerHTML = `
+        <div>Fibre <span class="accent">${fibre}<span> is  <span class="${fibreColoursEight[fibreColour]}-fibre inline-span">${fibreColoursEight[fibreColour]}</span></div>
+        <div>and is in ELEMENT <span class="accent">${elementValue}</span></div>
+        <div class="colour-display">${coloursHtml}</div>`;
+  }
+}
+
 function getElementNumber() {
   let coloursHtml = fibreColours
     .map((fibreColour, index) => {
@@ -54,13 +82,45 @@ function getElementNumber() {
       <div class="colour-display">${coloursHtml}</div>`;
   }
 }
+// Reset Content
+
+function resetValues() {
+  fibreNumber.value = "";
+  elementNumber.value = "";
+  displayElement.innerHTML = "";
+  displayFibre.innerHTML = "";
+}
 
 // Event Listeners
 
 fibreNumber.addEventListener("keyup", function () {
-  getFibreNumber();
+  fibreNumber.value === "" ? resetValues() : null;
+
+  if (fibreSelect === 12) {
+    getFibreNumber();
+  } else if (fibreSelect === 8) {
+    getFibreNumberEight();
+  }
 });
 
 elementNumber.addEventListener("keyup", function () {
   getElementNumber();
+});
+
+document.getElementById("reset").addEventListener("click", () => {
+  resetValues();
+});
+
+twelveFibre.addEventListener("click", function () {
+  fibreSelect = 12;
+  resetValues();
+  eightFibre.classList.remove("active");
+  twelveFibre.classList.add("active");
+});
+
+eightFibre.addEventListener("click", function () {
+  fibreSelect = 8;
+  resetValues();
+  eightFibre.classList.add("active");
+  twelveFibre.classList.remove("active");
 });
